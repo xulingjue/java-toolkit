77dc68327318e094d02801ca8a925a511bc45c4c
许凌绝 <lingjue.xu@qq.com>
2017-03-06 18:06:00 +0800

    add singleton patterns

---
+++ src/main/java/io/github/xulingjue/designpatterns/singleton/Play.java
@@ -0,0 +1,15 @@
+package io.github.xulingjue.designpatterns.singleton;
+
+/**
+ * Play
+ *
+ * @author lingjue.xu
+ * @Description:
+ * @date 2017/3/6 18:03
+ * @Version V1.0
+ */
+public class Play {
+    public static void main(String [] agrs){
+        Singleton2 singleton2 = Singleton2.getInstance();
+    }
+}
